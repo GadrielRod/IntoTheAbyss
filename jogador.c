b184888365268c58a7cc840a91b550f468c59f81
Gabriel Rodrigues de Rodrigues <62313869+GadrielRod@users.noreply.github.com>
2025-12-08 23:04:03 -0300

    Atualizar nos status do jogador.c
    As classes passaram por um balanceamento nos seus status, buscando um maior equilíbrio.

--- jogador.c
+++ jogador.c
@@ -1,6 +1,6 @@
 /*
  * JOGADOR.C
- * Gerencia status e c·lculos
+ * Gerencia status e c√°lculos
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,16 +20,16 @@ Jogador* jogador_criar(const char* nome, TipoClasse classe_escolhida) {
     // --- VALORES BASE ---
     int hp, atk, def, dano;
     switch (classe_escolhida) {
-        case CLASSE_GUERREIRO:  hp = 24; atk = 4; def = 12; dano = 6; break;
+        case CLASSE_GUERREIRO:  hp = 24; atk = 4; def = 11; dano = 6; break;
         case CLASSE_NECROMANTE: hp = 18; atk = 3; def = 10; dano = 15; break;
-        case CLASSE_SACERDOTE:  hp = 36; atk = 2; def = 11; dano = 4; break;
-        case CLASSE_HUNTERS:    hp = 18; atk = 6; def = 12;  dano = 5; break;
+        case CLASSE_SACERDOTE:  hp = 36; atk = 3; def = 10; dano = 4; break;
+        case CLASSE_HUNTERS:    hp = 21; atk = 4; def = 11;  dano = 5; break;
     }
 
     j->base = entidade_criar(nome, classe_escolhida, hp, atk, def, dano, 1);
     return j;
 }
-// FunÁ„o destrutora: Evita Memory Leaks
+// Fun√ß√£o destrutora: Evita Memory Leaks 
 void jogador_destruir(Jogador* j) {
     if (!j) return;
     entidade_liberar(j->base);
@@ -57,28 +57,28 @@ void jogador_equipar_item(Jogador* j, Item* i) {
 
 void jogador_usar_pocao(Jogador* j, Item* i) {
     if (i->tipo != ITEM_POCAO) return;
-
+    
     if (strstr(i->nome, "Furia") != NULL) {
         printf(">> Voce bebeu a POCAO DE FURIA! (+3 Dano neste combate)\n");
         j->base->buff_dano += 3;
     } else {
         printf(">> Voce bebeu a POCAO DE VIDA! (+10 HP)\n");
-        j->base->hp_atual += 10;
+        j->base->hp_atual += 10; 
         // Verifica se passou do Max HP calculado
         int max_hp = jogador_calcular_hp_max_total(j);
-        if (j->base->hp_atual > max_hp)
+        if (j->base->hp_atual > max_hp) 
             j->base->hp_atual = max_hp;
     }
-
+    
     lista_remover(j->inventario, i);
     item_liberar(i);
 }
 
-// --- C¡LCULOS DIN¬MICOS ---
+// --- C√ÅLCULOS DIN√ÇMICOS ---
 
 int jogador_calcular_hp_max_total(Jogador* j) {
     int total = j->base->hp_max;
-    // Soma itens que d„o HP
+    // Soma itens que d√£o HP
     if (j->arma_equipada && j->arma_equipada->status_afetado == STATUS_HP)
         total += j->arma_equipada->valor;
     if (j->armadura_equipada && j->armadura_equipada->status_afetado == STATUS_HP)
@@ -88,13 +88,13 @@ int jogador_calcular_hp_max_total(Jogador* j) {
 
 int jogador_calcular_dano_total(Jogador* j) {
     int total = j->base->dano_base;
-
-    // Soma itens que d„o Dano
+    
+    // Soma itens que d√£o Dano
     if (j->arma_equipada && j->arma_equipada->status_afetado == STATUS_DANO)
         total += j->arma_equipada->valor;
     if (j->armadura_equipada && j->armadura_equipada->status_afetado == STATUS_DANO)
         total += j->armadura_equipada->valor;
-
+        
     // Aliado Guerreiro
     if (j->aliado_equipado && strstr(j->aliado_equipado->nome, "Guerreiro"))
         total += 3;
@@ -105,28 +105,28 @@ int jogador_calcular_dano_total(Jogador* j) {
 
 int jogador_calcular_ataque_total(Jogador* j) {
     int total = j->base->ataque;
-
-    // Soma itens que d„o Ataque (Acerto)
+    
+    // Soma itens que d√£o Ataque (Acerto)
     if (j->arma_equipada && j->arma_equipada->status_afetado == STATUS_ATAQUE)
         total += j->arma_equipada->valor;
     if (j->armadura_equipada && j->armadura_equipada->status_afetado == STATUS_ATAQUE)
         total += j->armadura_equipada->valor;
-
-    // Aliado CaÁador
-    if (j->aliado_equipado && strstr(j->aliado_equipado->nome, "CaÁador"))
+        
+    // Aliado Ca√ßador
+    if (j->aliado_equipado && strstr(j->aliado_equipado->nome, "Ca√ßador"))
         total += 3;
-
+        
     return total;
 }
 
 int jogador_calcular_defesa_total(Jogador* j) {
     int total = j->base->defesa;
-
-    // Soma itens que d„o Defesa
+    
+    // Soma itens que d√£o Defesa
     if (j->arma_equipada && j->arma_equipada->status_afetado == STATUS_DEFESA)
         total += j->arma_equipada->valor;
     if (j->armadura_equipada && j->armadura_equipada->status_afetado == STATUS_DEFESA)
         total += j->armadura_equipada->valor;
-
+        
     return total;
 }
